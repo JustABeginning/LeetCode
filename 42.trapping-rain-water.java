ab6de58aafe07dbfd555e7761e8a5e18aa41e785
JustABeginning <jab.askme@gmail.com>
2022-09-18 22:23:01 +0530

    Hard

---
+++ 42.trapping-rain-water.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int ans = 0, left_max = 0, right_max = height[right];
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= left_max)
+                    left_max = height[left];
+                else
+                    ans += left_max - height[left];
+                left++;
+            } else {
+                if (height[right] >= right_max)
+                    right_max = height[right];
+                else
+                    ans += right_max - height[right];
+                right--;
+            }
+        }
+        return ans;
+    }
+}
