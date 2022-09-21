67664c490fb842437a63cf9fe25f3085b3d9154d
CoderJAB <jab.askme@gmail.com>
2022-09-21 07:22:33 +0530

    Time: 12 ms (18.01%), Space: 50.3 MB (95.50%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -1,13 +1,26 @@
+import java.util.HashMap;
 class Solution {
     public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
         int l=nums.length,r=queries.length;
+        int s=0;
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for(int j=0;j<l;j++){
+            if((nums[j]&1)==0){
+                s+=nums[j];
+                map.put(j,nums[j]);
+            }
+        }
         int[] arr=new int[r];
         for(int i=0;i<r;i++){
             nums[queries[i][1]]+=queries[i][0];
-            int s=0;
-            for(int j=0;j<l;j++)
-                if((nums[j]&1)==0)
-                    s+=nums[j];
+            if(map.containsKey(queries[i][1])){
+                s-=map.get(queries[i][1]);
+                map.remove(queries[i][1]);
+            }
+            if((nums[queries[i][1]]&1)==0){
+                s+=nums[queries[i][1]];
+                map.put(queries[i][1],nums[queries[i][1]]);
+            }
             arr[i]=s;
         }
         return arr;
