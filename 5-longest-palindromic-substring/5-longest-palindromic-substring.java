f0a67a53e23580520b6648228afee993e611d8f5
CoderJAB <jab.askme@gmail.com>
2022-09-21 21:46:36 +0530

    Time: 2351 ms (5.01%), Space: 533 MB (5.01%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        String res="";
+        int l=s.length();
+        if(l>1){
+            int skp=1;
+            outer:
+            for(int i=0;i<l;i++){
+                for(int j=i+skp;j<=l;j++){
+                    String t=s.substring(i,j);
+                    int n=t.length();
+                    if(isPalin(t)&&n>res.length()){
+                        res=t;
+                        if(n==l)
+                            break outer;
+                        skp=n;
+                    }
+                }
+            }
+        }
+        else
+            res=s;
+        return res;
+    }
+    private boolean isPalin(String s){
+        boolean res=true;
+        int l=s.length(),m=l/2;
+        for(int i=0;i<m;i++){
+            if(s.charAt(i)!=s.charAt(l-1-i)){
+                res=false;
+                break;
+            }
+        }
+        return res;
+    }
+}
