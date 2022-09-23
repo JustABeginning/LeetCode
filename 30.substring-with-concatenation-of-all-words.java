d7d0527f240dd024b4c47366a2d07feff819ae84
CoderJAB <jab.askme@gmail.com>
2022-09-24 00:57:04 +0530

    update 30.substring-with-concatenation-of-all-words.java

--- 30.substring-with-concatenation-of-all-words.java
+++ 30.substring-with-concatenation-of-all-words.java
@@ -1,19 +1,19 @@
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 class Solution {
+    private int n, d;
 
     public List<Integer> findSubstring(String s, String[] words) {
-        List<String> set = new ArrayList<String>();
-        for (int k = 0; k < words.length; k++)
-            set.add(words[k]);
-        Collections.sort(set);
-        int i = 0, d = words[0].length(), skp = words.length * d, j = i + skp;
+        n = words.length;
+        d = words[0].length();
+        Arrays.sort(words);
+        int i = 0, skp = n * d, j = i + skp;
         List<Integer> ans = new ArrayList<Integer>();
         while (j <= s.length()) {
             String t = s.substring(i, j);
-            if (isPermute(t, d, set))
+            if (isPermute(t, words))
                 ans.add(i);
             i++;
             j = i + skp;
@@ -21,15 +21,15 @@ class Solution {
         return ans;
     }
 
-    private boolean isPermute(String s, int d, List<String> st) {
-        List<String> ts = new ArrayList<String>();
-        int i = 0, j = i + d;
+    private boolean isPermute(String s, String[] arr) {
+        String[] tmp = new String[n];
+        int i = 0, j = i + d, k = 0;
         while (j <= s.length()) {
-            ts.add(s.substring(i, j));
+            tmp[k++] = s.substring(i, j);
             i = j;
             j = i + d;
         }
-        Collections.sort(ts);
-        return ts.equals(st);
+        Arrays.sort(tmp);
+        return Arrays.equals(tmp, arr);
     }
 }
