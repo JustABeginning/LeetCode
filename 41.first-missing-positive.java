a010f0afd5413410ff4dd9092fcedcc1b5c1c976
CoderJAB <jab.askme@gmail.com>
2022-09-24 18:00:13 +0530

    update 41.first-missing-positive.java

--- 41.first-missing-positive.java
+++ 41.first-missing-positive.java
@@ -2,19 +2,16 @@ import java.util.TreeSet;
 
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int min = 0;
         TreeSet<Integer> set = new TreeSet<Integer>();
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                if (min == 0 || nums[i] < min)
-                    min = nums[i];
+            if (nums[i] > 0)
                 set.add(nums[i]);
-            }
+
         }
-        if (min > 1)
+        if (set.isEmpty() || set.first() > 1)
             return 1;
         else {
-            int prev = min;
+            int prev = set.first();
             for (int x : set) {
                 if ((x - prev) > 1)
                     break;
