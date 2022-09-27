49d681cc1dd0eb74709dc3274b72c1e278d9a153
CoderJAB <jab.askme@gmail.com>
2022-09-27 15:11:07 +0530

    update 1909.remove-one-element-to-make-the-array-strictly-increasing.java

--- 1909.remove-one-element-to-make-the-array-strictly-increasing.java
+++ 1909.remove-one-element-to-make-the-array-strictly-increasing.java
@@ -1,25 +1,13 @@
 class Solution {
     public boolean canBeIncreasing(int[] nums) {
-        boolean res = false;
-        for (int i = 0; i < nums.length; i++) {
-            if (checkIncr(nums, i)) {
-                res = true;
-                break;
+        int c = 0;
+        for (int i = 1; i < nums.length && c < 2; i++) {
+            if (nums[i - 1] >= nums[i]) {
+                c++;
+                if (i > 1 && nums[i - 2] >= nums[i])
+                    nums[i] = nums[i - 1];
             }
         }
-        return res;
-    }
-
-    private boolean checkIncr(int[] arr, int k) {
-        int n = arr.length - 1;
-        for (int i = 0, j = i + 1; i < n; i = j, j++) {
-            if (i == k)
-                continue;
-            if (j == k)
-                j = k + 1;
-            if (j <= n && arr[i] >= arr[j])
-                return false;
-        }
-        return true;
+        return c < 2;
     }
 }
