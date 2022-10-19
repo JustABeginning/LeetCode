cec96067db34f276b934fa7a689efdb91a3f36e2
CoderJAB <jab.askme@gmail.com>
2022-10-19 12:11:51 +0530

    update 692.top-k-frequent-words.java

---
+++ 692.top-k-frequent-words.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Arrays.sort(words);
+        TreeMap<String, Integer> map = new TreeMap<String, Integer>();
+        for (int i = 0, c = 0; i < words.length; i++) {
+            c = map.getOrDefault(words[i], 0);
+            map.put(words[i], c + 1);
+        }
+        List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(map.entrySet());
+        Comparator<Entry<String, Integer>> comp = new Comparator<Entry<String, Integer>>() {
+            @Override
+            public int compare(Entry<String, Integer> e1, Entry<String, Integer> e2) {
+                return e2.getValue() - e1.getValue();
+            }
+        };
+        Collections.sort(list, comp);
+        List<String> ret = new ArrayList<String>(k);
+        int c = 0;
+        for (Entry<String, Integer> e : list) {
+            ret.add(e.getKey());
+            c++;
+            if (c == k)
+                break;
+        }
+        return ret;
+    }
+}
